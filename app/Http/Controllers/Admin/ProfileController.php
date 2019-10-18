97109c64f21e5ad1b9bb456e97b5066490cf5e11
Yuya Kuboi <y9boit@icloud.com>
2019-10-18 09:23:40 +0000

    P/L14.2task

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,20 @@ class ProfileController extends Controller
     
     public function create()
     {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'gender' => 'required',
+            'hobby' => 'required',
+            'introduction' => 'required',
+        ]);
+        
+        $profile = new Profile();
+        $profile->name = $validatedData['name'];
+        $profile->gender = $validatedData['gender'];
+        $profile->hobby = $validatedData['hobby'];
+        $profile->introduction = $validatedData['introduction'];
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
     
