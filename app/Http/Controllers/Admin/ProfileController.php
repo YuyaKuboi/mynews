648af7a49077682d5efde68580be0523fa9d07e6
Yuya Kuboi <y9boit@icloud.com>
2019-10-25 08:36:56 +0000

    P/L16task

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,29 +16,46 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'gender' => 'required',
-            'hobby' => 'required',
-            'introduction' => 'required',
-        ]);
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
         
-        $profile = new Profile();
-        $profile->name = $validatedData['name'];
-        $profile->gender = $validatedData['gender'];
-        $profile->hobby = $validatedData['hobby'];
-        $profile->introduction = $validatedData['introduction'];
+        $profile->fill($form);
         $profile->save();
         
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            $posts = Profile::where('name', $cond_name)->get();
+        }else{
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
     }
-    public function update()
+    
+    public function edit(Request $request)
     {
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    public function update(Request $request)
+    {
+        $this->validation($request, Profile::$rules);
+        
+        $profile = Profile::find($request->id);
+        
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        
+        $profile->fill($profile_form)->save();
+        
         return redirect('admin/profile/edit');
     }
     
